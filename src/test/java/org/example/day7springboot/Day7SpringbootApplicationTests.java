4f4dc364e19f1f66c17f4a6712dc19be8f32a6fc
BIN <BIN.JIAN@OOCL.COM>
2025-09-09 20:24:52 +0800

    test: add test for API getEmployee by gender

--- src/test/java/org/example/day7springboot/Day7SpringbootApplicationTests.java
+++ src/test/java/org/example/day7springboot/Day7SpringbootApplicationTests.java
@@ -49,9 +49,7 @@ class Day7SpringbootApplicationTests {
       """;
     mockMvc.perform(post("/employees")
         .contentType(MediaType.APPLICATION_JSON)
-        .content(requestBody))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.id").value(1));
+        .content(requestBody));
 
     mockMvc.perform(get("/employees/{id}", 1)
         .contentType(MediaType.APPLICATION_JSON))
@@ -62,5 +60,30 @@ class Day7SpringbootApplicationTests {
         .andExpect(jsonPath("$.gender").value("MALE"))
         .andExpect(jsonPath("$.salary").value(6000.0));
   }
+
+  @Test
+  void should_get_a_employee_when_give_gender_then_return_a_employee() throws Exception {
+    String requestBody = """
+      {
+        "name":"John",
+        "age": 30,
+        "gender": "male",
+        "salary": 6000
+      }
+      """;
+    mockMvc.perform(post("/employees")
+      .contentType(MediaType.APPLICATION_JSON)
+      .content(requestBody));
+    mockMvc.perform(get("/employees/gender?gender=male")
+        .contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$[0].id").value(1))
+      .andExpect(jsonPath("$[0].name").value("John"))
+      .andExpect(jsonPath("$[0].age").value(30))
+      .andExpect(jsonPath("$[0].gender").value("male"))
+      .andExpect(jsonPath("$[0].salary").value(6000.0));
+
+  }
+
 
 }
